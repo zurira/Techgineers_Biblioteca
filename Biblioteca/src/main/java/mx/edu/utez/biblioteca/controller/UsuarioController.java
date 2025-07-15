920b2209e2113320b056bcd993af357bca50fffb
zurira <20243ds036@utez.edu.mx>
2025-07-14 22:35:55 -0600

    Agregue dos métodos al UsuarioController OnLogin donde se valida y se muestra si sus credenciales son válidas, asi mismo relice unas alertas para que se muestren en caso contrario

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/UsuarioController.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/UsuarioController.java
@@ -1,5 +1,60 @@
 package mx.edu.utez.biblioteca.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import mx.edu.utez.biblioteca.dao.impl.UsuarioDaoImpl;
+
 public class UsuarioController {
-    
+    @FXML
+    private TextField txtcorreo;
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private void onLogin(ActionEvent e){
+        String correo=txtcorreo.getText().trim();
+        String pass=txtPassword.getText().trim();
+
+        UsuarioDaoImpl dao=new UsuarioDaoImpl();
+        try {
+            if(dao.login(correo,pass)){
+                System.out.println("Se pudo logear con Exito!");
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/mx/edu/utez/demo/view/dashboard.fxml"));
+                Scene scene = new Scene(loader.load());
+                Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+                stage.setScene(scene);
+
+
+            }else{
+                showAlert("Error","Credenciales incorrectas");
+                System.out.println("Credenciales incorrectas!");
+            }
+
+        }catch (Exception err){
+            showAlert("Error", "Hubo un error en la aplicación");
+            System.out.println(err.getMessage());
+        }
+
+    }
+
+    @FXML
+    private void onTest(ActionEvent e){
+
+    }
+
+    public void showAlert(String title, String msg){
+        Alert alert=new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(msg);
+        alert.showAndWait();
+    }
+
 }
