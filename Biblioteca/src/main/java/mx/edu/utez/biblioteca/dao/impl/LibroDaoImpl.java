85deb0e317f40d53fb6a8a9ce3bd8adb6377d198
zurira <20243ds036@utez.edu.mx>
2025-07-20 10:48:02 -0600

    Se incio con el método obtenerLibros, donde se crea una lista para almacenarlos, se agrego el query que obtiene la información necesaria de los libros

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/impl/LibroDaoImpl.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/impl/LibroDaoImpl.java
@@ -3,11 +3,30 @@ package mx.edu.utez.biblioteca.dao.impl;
 import mx.edu.utez.biblioteca.dao.ILibro;
 import mx.edu.utez.biblioteca.model.Libro;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LibroDaoImpl implements ILibro {
     @Override
     public List<Libro> obtenerLibros() {
+        List<Libro> libros = new ArrayList<>();
+        String sql = """
+                SELECT 
+                    l.ID, 
+                    l.TITULO, 
+                    l.ANIO_PUBLICACION,
+                    l.PORTADA,
+                    e.NOMBRE AS EDITORIAL,
+                    (
+                        SELECT LISTAGG(a.NOMBRE_COMPLETO, ', ') 
+                        WITHIN GROUP (ORDER BY a.NOMBRE_COMPLETO)
+                        FROM LIBRO_AUTOR la
+                        JOIN AUTOR a ON la.ID_AUTOR = a.ID
+                        WHERE la.ID_LIBRO = l.ID
+                    ) AS AUTORES
+                FROM LIBRO l
+                LEFT JOIN EDITORIAL e ON l.ID_EDITORIAL = e.ID
+                """;
         return List.of();
     }
 }
