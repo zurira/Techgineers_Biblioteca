67c9541d543bbe924f770052edeaca6afdd13a73
zurira <20243ds036@utez.edu.mx>
2025-07-20 03:55:45 -0600

    Se agregaron todos los atributos a la clase Libro

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Libro.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Libro.java
@@ -2,5 +2,10 @@ package mx.edu.utez.biblioteca.model;
 
 public class Libro {
     private int id;
-    
+    private String titulo;
+    private String isbn;
+    private String resumen;
+    private int anioPublicacion;
+    private String portada;
+    private int idEditorial;
 }
