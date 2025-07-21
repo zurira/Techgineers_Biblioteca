3e8b93fa82e065b1b6d2305648d68fa8ce9d12f2
zurira <20243ds036@utez.edu.mx>
2025-07-21 16:35:21 -0600

    Se generaron los getters y stters de la clase Categoria

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Categoria.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Categoria.java
@@ -3,4 +3,20 @@ package mx.edu.utez.biblioteca.model;
 public class Categoria {
     private int id;
     private String nombre;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
