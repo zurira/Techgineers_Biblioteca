a6dcafbcbd868eac1fba05c6b9bcdea56fc4e4fb
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-21 14:24:42 -0600

    Agregada clase SuperAdminApp con estructura básica del modelo

---
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/SuperAdminApp.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class SuperAdminApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/superadmin-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(getClass().getResource("/style/superadmin-style.css").toExternalForm());
+        stage.setTitle("Panel de SuperAdministrador");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
