eadfc1dd2d43f3d9b815db04c56fe53006ef3e40
zurira <20243ds036@utez.edu.mx>
2025-07-21 16:32:31 -0600

    Se hicieron los getters y setters de la clase Autor

---
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Autor.java
@@ -0,0 +1,22 @@
+package mx.edu.utez.biblioteca.model;
+
+public class Autor {
+    private int id;
+    private String nombreCompleto;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+}
