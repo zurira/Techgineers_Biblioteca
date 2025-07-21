d3de49620b8737eae8d5d65ab2ded39306a8dedc
zurira <20243ds036@utez.edu.mx>
2025-07-20 21:38:23 -0600

    Se creo una instancia de la clase LibroDaoImpl y se enlao el elemnto FlowPane

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/BienvenidaController.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/BienvenidaController.java
@@ -1,5 +1,14 @@
 package mx.edu.utez.biblioteca.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.FlowPane;
+import mx.edu.utez.biblioteca.dao.impl.LibroDaoImpl;
+
 public class BienvenidaController {
+    LibroDaoImpl libroDao = new LibroDaoImpl();
+
+    @FXML private FlowPane contenedorLibros;
+
+
 }
 
