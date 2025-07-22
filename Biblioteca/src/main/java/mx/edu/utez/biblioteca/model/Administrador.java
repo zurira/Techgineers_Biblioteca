91109774bee980c6ac053c18fe673ba6e4f7a1d0
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-22 14:49:23 -0600

    Coloque el metodo de la clase Administrador

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Administrador.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Administrador.java
@@ -9,6 +9,15 @@ public class Administrador {
     private boolean estado;
 
     
-}
+        public Administrador(int id, String nombreCompleto, String usuario, String correo, boolean estado) {
+            this.id = id;
+            this.nombreCompleto = nombreCompleto;
+            this.usuario = usuario;
+            this.correo = correo;
+            this.estado = estado;
+        }
+    }
+
+
 
 
