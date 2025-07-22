0f725cfa33804611539e8d1868046ef2cb94685d
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-22 14:48:26 -0600

    Coloque los atributos de la clase Administrador

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Administrador.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Administrador.java
@@ -1,5 +1,14 @@
 package mx.edu.utez.biblioteca.model;
 
 
+public class Administrador {
+    private int id;
+    private String nombreCompleto;
+    private String usuario;
+    private String correo;
+    private boolean estado;
+
+    
+}
 
 
