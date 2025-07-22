7b177d752f8594d14a909f3919c9de00c2c52746
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-22 14:51:22 -0600

    Modifique el archivo para despues hacer la coneccion de la base de datos

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/config/DBConnection.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/config/DBConnection.java
@@ -1,4 +1,26 @@
 package mx.edu.utez.biblioteca.config;
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    public static Connection getConnection() {
+        try {
+            String alias = "bdutez_high"; // Usa el alias definido en tu tnsnames.ora
+            String tnsAdminPath = "C:\\Users\\letic\\Downloads\\Wallet_L9I11J33YFKVI0T1"; // ruta donde se descomprime la wallet
+
+            String url = "jdbc:oracle:thin:@"+alias+"?TNS_ADMIN="+tnsAdminPath;
+            String user = "L9I11J33YFKVI0T1"; //  usuario
+            String pass = "Techgineers123"; //  contraseña
+
+            return DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            System.err.println(" Error al conectar a Oracle con Wallet:");
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
