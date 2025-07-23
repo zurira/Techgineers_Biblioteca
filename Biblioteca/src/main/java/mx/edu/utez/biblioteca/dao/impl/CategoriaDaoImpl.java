cf07e5fe4db8a6622bf5a222ac71abdfe32e043e
zurira <20243ds036@utez.edu.mx>
2025-07-22 19:37:13 -0600

    Se incio con la implementación de la clase CategoriaDaoImpl, creando el método obtenerNombresCategorias heredado de ICategoria

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/impl/CategoriaDaoImpl.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/impl/CategoriaDaoImpl.java
@@ -1,4 +1,12 @@
 package mx.edu.utez.biblioteca.dao.impl;
 
-public class CategoriaDaoImpl {
+import mx.edu.utez.biblioteca.dao.ICategoria;
+
+import java.util.List;
+
+public class CategoriaDaoImpl implements ICategoria {
+    @Override
+    public List<String> obtenerNombresCategorias() {
+        return List.of();
+    }
 }
