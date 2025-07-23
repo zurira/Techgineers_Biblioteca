4d50eea240ce4a31a748ef5c50d68156e7d8f954
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-23 14:18:04 -0600

    Puse la clase MainModal, la termine

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/MainModal.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/MainModal.java
@@ -8,4 +8,23 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
+public class MainModal extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/mx/edu/utez/biblioteca/views/ModalAgregarAdministrador.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("Agregar Administrador");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
+
 
