d9cd3c1321f3decc8e252655049da3ff1501a00f
zurira <20243ds036@utez.edu.mx>
2025-07-22 19:48:09 -0600

    Se creo un try catch, donde se  establece y prepara la conexión y luego se ejecuta el query y se va obteniendo el nombre de las categorías

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/impl/CategoriaDaoImpl.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/impl/CategoriaDaoImpl.java
@@ -1,7 +1,11 @@
 package mx.edu.utez.biblioteca.dao.impl;
 
+import mx.edu.utez.biblioteca.config.DBConnection;
 import mx.edu.utez.biblioteca.dao.ICategoria;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +14,17 @@ public class CategoriaDaoImpl implements ICategoria {
     public List<String> obtenerNombresCategorias() {
         List<String> categorias = new ArrayList<>();
         String sql = "SELECT NOMBRE FROM CATEGORIA ORDER BY NOMBRE";
+        try {
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                categorias.add(rs.getString("NOMBRE"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return categorias;
 
