183f023486be963f8d6bb731f537136c0685cf4c
zurira <20243ds036@utez.edu.mx>
2025-07-22 18:08:37 -0600

    Se inicio con la implementación de BusquedaController, enlazando sus componentes como son TextFiel, ComboBox y FlowPane, así mismo se instancio la clase LibroDaoImpl, ya que sera de utilidad para cargar los libros y las categorías

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/BusquedaController.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/BusquedaController.java
@@ -1,4 +1,15 @@
 package mx.edu.utez.biblioteca.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import mx.edu.utez.biblioteca.dao.impl.LibroDaoImpl;
+
 public class BusquedaController {
+    @FXML private TextField txtBuscar;
+    @FXML private ComboBox<String> cmbCategoria;
+    @FXML private FlowPane contenedorResultados;
+
+    LibroDaoImpl libroDao = new LibroDaoImpl();
 }
