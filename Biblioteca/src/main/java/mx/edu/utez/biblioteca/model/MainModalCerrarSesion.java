7b568968ebe18740b44707cd459886db1d212db4
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-23 15:47:33 -0600

    Coloque las respectivas librerias

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/MainModalCerrarSesion.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/MainModalCerrarSesion.java
@@ -1,3 +1,9 @@
 package mx.edu.utez.biblioteca.model;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 
