87a3c273c04b72836ceee5dbfbcad08329920ffb
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-23 15:50:44 -0600

    Culmino la clase

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/MainModalCerrarSesion.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/MainModalCerrarSesion.java
@@ -6,4 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+public class MainModalCerrarSesion extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/mx/edu/utez/biblioteca/views/ModalCerrarSesion.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Cerrar sesión");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
 
