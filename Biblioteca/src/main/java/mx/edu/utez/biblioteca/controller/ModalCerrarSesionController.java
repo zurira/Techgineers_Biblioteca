39b74a53e2fdcffe9ef78988ccb09c46b73fb86a
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-23 15:44:46 -0600

    Comente el archivo

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/ModalCerrarSesionController.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/ModalCerrarSesionController.java
@@ -3,5 +3,5 @@ package mx.edu.utez.biblioteca.controller;
 
 
 public class ModalCerrarSesionController {
-    
+    //aqui voy a poner la logica que se va a seguir para el correcto funcionamiento de los botones
 }
