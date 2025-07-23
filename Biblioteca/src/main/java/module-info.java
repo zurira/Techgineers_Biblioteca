e11e90e9a840b73e3cc6126d5d4024727a3c0642
vivisahe <20243ds058@utez.edu.mx>
2025-07-23 17:29:20 -0600

    Module modificado

--- Biblioteca/src/main/java/module-info.java
+++ Biblioteca/src/main/java/module-info.java
@@ -1,12 +1,18 @@
 module mx.edu.utez.biblioteca {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.graphics;
+    requires java.sql;
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
-    requires java.sql;
 
-    opens mx.edu.utez.biblioteca to javafx.fxml;
-    exports mx.edu.utez.biblioteca;
+    opens mx.edu.utez.biblioteca to javafx.graphics;
     opens mx.edu.utez.biblioteca.controller to javafx.fxml;
+
+    exports mx.edu.utez.biblioteca;
+    exports mx.edu.utez.biblioteca.controller;
+
+    exports mx.edu.utez.biblioteca.model;
+    opens mx.edu.utez.biblioteca.model to javafx.fxml;
+
 }
