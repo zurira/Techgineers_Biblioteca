a5d62ebf31d1e838578f44796ddefb1606316da5
zurira <20243ds036@utez.edu.mx>
2025-07-22 19:44:47 -0600

    Se creo una lista de categorias y se declaro una consulta la cual muestra las categorias ordenadas de forma alfabetica

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/impl/CategoriaDaoImpl.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/impl/CategoriaDaoImpl.java
@@ -2,11 +2,16 @@ package mx.edu.utez.biblioteca.dao.impl;
 
 import mx.edu.utez.biblioteca.dao.ICategoria;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CategoriaDaoImpl implements ICategoria {
     @Override
     public List<String> obtenerNombresCategorias() {
-        return List.of();
+        List<String> categorias = new ArrayList<>();
+        String sql = "SELECT NOMBRE FROM CATEGORIA ORDER BY NOMBRE";
+
+        return categorias;
+
     }
 }
