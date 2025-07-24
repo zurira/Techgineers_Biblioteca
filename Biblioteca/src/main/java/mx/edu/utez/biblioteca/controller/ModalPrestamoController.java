8449069742efb37e4a03bd4f5887cf63710bfefd
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-24 09:26:36 -0600

    Hice el controller del modal agregar prestamo

---
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/ModalPrestamoController.java
@@ -0,0 +1,19 @@
+package mx.edu.utez.biblioteca.controller;
+
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class ModalPrestamoController {
+
+    @FXML private TextField txtNombre;
+    @FXML private TextField txtUsuario;
+    @FXML private TextField txtCorreo;
+    @FXML private TextField txtEstado;
+    @FXML private DatePicker datePrestamo;
+    @FXML private DatePicker dateLimite;
+    @FXML private DatePicker dateEntrega;
+
+    // Aquí podrías agregar métodos para guardar o cancelar después
+}
