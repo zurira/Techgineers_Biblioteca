27fca0a7f7add4a5f7c0be0fda5da2c3cadadcba
Daniela <20243ds043@utez.edu.mx>
2025-07-24 17:37:25 -0600

    Se definieron los atributos de la tabla prestamos

---
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Prestamo.java
@@ -0,0 +1,11 @@
+package mx.edu.utez.biblioteca.model;
+
+public class Prestamo {
+    private Integer no;
+    private String nombreCompleto;
+    private String correo;
+    private String fechaPrestamo;
+    private String fechaLimite;
+    private String fechaReal;
+    private String estado;
+}
