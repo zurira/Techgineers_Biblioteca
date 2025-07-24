b45dfcebdb5009019751ed9438a29127f54f9b08
Daniela <20243ds043@utez.edu.mx>
2025-07-24 17:42:53 -0600

    Se agregaron los getters y setters de prestamos

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Prestamo.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Prestamo.java
@@ -8,4 +8,60 @@ public class Prestamo {
     private String fechaLimite;
     private String fechaReal;
     private String estado;
+
+    public Integer getNo() {
+        return no;
+    }
+
+    public void setNo(Integer no) {
+        this.no = no;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(String fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public String getFechaLimite() {
+        return fechaLimite;
+    }
+
+    public void setFechaLimite(String fechaLimite) {
+        this.fechaLimite = fechaLimite;
+    }
+
+    public String getFechaReal() {
+        return fechaReal;
+    }
+
+    public void setFechaReal(String fechaReal) {
+        this.fechaReal = fechaReal;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
