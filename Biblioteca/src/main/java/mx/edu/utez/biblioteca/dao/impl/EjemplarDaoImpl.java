5d7495b061db563ac5d04695c9ff140bd923c5e7
zurira <20243ds036@utez.edu.mx>
2025-07-25 07:30:39 -0600

    Se creo el método buscarEjemplaresDisponibles el cual ayudara a encontrar de manera más rapida el libro solicitado

---
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/impl/EjemplarDaoImpl.java
@@ -0,0 +1,54 @@
+package mx.edu.utez.biblioteca.dao.impl;
+
+import mx.edu.utez.biblioteca.config.DBConnection;
+import mx.edu.utez.biblioteca.model.Ejemplar;
+
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+
+public class EjemplarDaoImpl {
+
+    /**
+     * Busca ejemplares disponibles que coincidan con un filtro de texto.
+     * El filtro puede aplicarse sobre el título o el código local.
+     */
+    public ObservableList<Ejemplar> buscarEjemplaresDisponibles(String filtro) {
+        ObservableList<Ejemplar> lista = FXCollections.observableArrayList();
+        String sql = "SELECT e.ID, e.CODIGO_LOCAL, l.TITULO, e.UBICACION " +
+                "FROM EJEMPLAR e " +
+                "JOIN LIBRO l ON e.ID_LIBRO = l.ID " +
+                "WHERE e.DISPONIBLE = 'S' AND (l.TITULO LIKE ? OR e.CODIGO_LOCAL LIKE ?)";
+
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement pst = con.prepareStatement(sql);
+
+            String query = "%" + filtro + "%";
+            pst.setString(1, query);
+            pst.setString(2, query);
+
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                Ejemplar ejemplar = new Ejemplar();
+
+                ejemplar.setIdEjemplar(rs.getInt("ID"));
+                ejemplar.setCodigo(rs.getString("CODIGO_LOCAL"));
+                ejemplar.setTitulo(rs.getString("TITULO"));
+                ejemplar.setUbicacion(rs.getString("UBICACION"));
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return lista;
+    }
+
+
+}
