3d8e3966a3b62c5d91a76d3880bb6ef5cf57b9e7
zurira <20243ds036@utez.edu.mx>
2025-07-25 07:38:17 -0600

    Se inicio con el modelo de DetallePrestamo

---
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/DetallePrestamo.java
@@ -0,0 +1,9 @@
+package mx.edu.utez.biblioteca.model;
+
+public class DetallePrestamo {
+    private String id;
+    private String id_prestamo;
+    private String id_ejemplar;
+    private boolean develto;
+
+}
