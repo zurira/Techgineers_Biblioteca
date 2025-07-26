ef0e058d8fd6b3b8d13019cad9d678d273447e2c
lizzy <20243ds052@utez.edu.mx>
2025-07-26 15:04:57 -0600

    cambios en el archivo DBConnection

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/config/DBConnection.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/config/DBConnection.java
@@ -7,12 +7,12 @@ public class DBConnection {
 
         private static final String URL      = "jdbc:oracle:thin:@f5n4s5zks6b1rbya_low";
         private static final String USER     = "ADMIN";
-        private static final String PASSWORD = "Lizzete275_*";
+        private static final String PASSWORD = "Techgineers123";
 
         // Obtiene una conexión nueva
         public static Connection getConnection() throws SQLException {
             // 1. Apunta al directorio donde descomprimiste el wallet
-            System.setProperty("oracle.net.tns_admin", "C:\\Users\\britt\\Downloads\\Wallet_F5N4S5ZKS6B1RBYA");
+            System.setProperty("oracle.net.tns_admin", "C:\\Users\\britt\\Downloads\\Wallet_L9I11J33YFKVI0T1");
             // 2. (Opcional) fuerza la validación de nombre de servidor en el certificado
             System.setProperty("oracle.net.ssl_server_dn_match", "true");
             // 3. Obtiene la conexión usando alias, user y pass
