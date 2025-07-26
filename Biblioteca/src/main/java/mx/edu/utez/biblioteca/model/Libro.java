3ebf8632109de5c6cc9214bc147d2985f276ec8d
vivisahe <20243ds058@utez.edu.mx>
2025-07-25 18:02:58 -0600

    Se agregaron getters y setters en la clase de Libro

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Libro.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Libro.java
@@ -1,4 +1,71 @@
 package mx.edu.utez.biblioteca.model;
 
 public class Libro {
+    private String NO;
+    private String ISBN;
+    private String titulo;
+    private String autor;
+    private String editorial;
+    private String descripcion;
+    private String categoria;
+
+    public Libro() {
+
+    }
+
+    public String getNO() {
+        return NO;
+    }
+
+    public void setNO(String NO) {
+        this.NO = NO;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
