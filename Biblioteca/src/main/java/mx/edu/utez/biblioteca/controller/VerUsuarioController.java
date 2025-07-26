cc9d37ea9d214ef5f68028f2746b6aafc2d25ca4
lizzy <20243ds052@utez.edu.mx>
2025-07-25 22:18:45 -0600

    implementación de métodos y declaración de variables en controller de ver usuario

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/VerUsuarioController.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/VerUsuarioController.java
@@ -1,5 +1,26 @@
 package mx.edu.utez.biblioteca.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
 public class VerUsuarioController {
 
-}
+        @FXML private TextField txtNombre, txtEmail, txtTelefono;
+        @FXML private DatePicker dateNacimiento;
+        @FXML private TextArea txtDireccion;
+
+        public void setDatos(String nombre, String email, String telefono, String direccion, String fecha) {
+            txtNombre.setText(nombre);
+            txtEmail.setText(email);
+            txtTelefono.setText(telefono);
+            txtDireccion.setText(direccion);
+            dateNacimiento.setPromptText(fecha);
+        }
+
+        @FXML
+        private void cerrar() {
+            Stage stage = (Stage) txtNombre.getScene().getWindow();
+            stage.close();
+        }
+    }
