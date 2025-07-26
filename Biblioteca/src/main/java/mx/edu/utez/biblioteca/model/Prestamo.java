7ee6857f56692a09600b44ed27a9c8a5f1ce3e8c
Daniela <20243ds043@utez.edu.mx>
2025-07-26 17:32:26 -0600

    Modificación de los atributos de la clase e implementación de metodo para una representación en forma de cadena

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Prestamo.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Prestamo.java
@@ -1,59 +1,74 @@
 package mx.edu.utez.biblioteca.model;
 
+import java.time.LocalDate;
+
 public class Prestamo {
-    private Integer no;
-    private String nombreCompleto;
-    private String correo;
-    private String fechaPrestamo;
-    private String fechaLimite;
-    private String fechaReal;
+    private int id;
+    private Libro libro;
+    private UsuarioBiblioteca usuario;
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaLimite;
+    private LocalDate fechaReal;
     private String estado;
 
-    public Integer getNo() {
-        return no;
+    public Prestamo() {
     }
 
-    public void setNo(Integer no) {
-        this.no = no;
+    public Prestamo(int id, Libro libro, UsuarioBiblioteca usuario, LocalDate fechaPrestamo, LocalDate fechaLimite, LocalDate fechaReal, String estado) {
+        this.id = id;
+        this.libro = libro;
+        this.usuario = usuario;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaLimite = fechaLimite;
+        this.fechaReal = fechaReal;
+        this.estado = estado;
     }
 
-    public String getNombreCompleto() {
-        return nombreCompleto;
+    public int getId() {
+        return id;
     }
 
-    public void setNombreCompleto(String nombreCompleto) {
-        this.nombreCompleto = nombreCompleto;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getCorreo() {
-        return correo;
+    public Libro getLibro() {
+        return libro;
     }
 
-    public void setCorreo(String correo) {
-        this.correo = correo;
+    public void setLibro(Libro libro) {
+        this.libro = libro;
     }
 
-    public String getFechaPrestamo() {
+    public UsuarioBiblioteca getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(UsuarioBiblioteca usuario) {
+        this.usuario = usuario;
+    }
+
+    public LocalDate getFechaPrestamo() {
         return fechaPrestamo;
     }
 
-    public void setFechaPrestamo(String fechaPrestamo) {
+    public void setFechaPrestamo(LocalDate fechaPrestamo) {
         this.fechaPrestamo = fechaPrestamo;
     }
 
-    public String getFechaLimite() {
+    public LocalDate getFechaLimite() {
         return fechaLimite;
     }
 
-    public void setFechaLimite(String fechaLimite) {
+    public void setFechaLimite(LocalDate fechaLimite) {
         this.fechaLimite = fechaLimite;
     }
 
-    public String getFechaReal() {
+    public LocalDate getFechaReal() {
         return fechaReal;
     }
 
-    public void setFechaReal(String fechaReal) {
+    public void setFechaReal(LocalDate fechaReal) {
         this.fechaReal = fechaReal;
     }
 
@@ -63,5 +78,18 @@ public class Prestamo {
 
     public void setEstado(String estado) {
         this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Prestamo{" +
+                "id=" + id +
+                ", libro=" + (libro != null ? libro.getTitulo() : "N/A") + // Asegura que no sea null
+                ", usuario=" + (usuario != null ? usuario.getNombre() : "N/A") + // Asegura que no sea null
+                ", fechaPrestamo=" + fechaPrestamo +
+                ", fechaLimite=" + fechaLimite +
+                ", fechaReal=" + fechaReal +
+                ", estado='" + estado + '\'' +
+                '}';
     }
 }
