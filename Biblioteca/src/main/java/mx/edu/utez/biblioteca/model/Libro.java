90269a40550a22a8b6e806a637dd7d08fb60ba5f
vivisahe <20243ds058@utez.edu.mx>
2025-07-25 18:10:15 -0600

    Se agregaron getters y setters en la clase de Libro

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Libro.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Libro.java
@@ -13,59 +13,31 @@ public class Libro {
 
     }
 
-    public String getNO() {
-        return NO;
-    }
+    public String getNO() { return NO;}
 
-    public void setNO(String NO) {
-        this.NO = NO;
-    }
+    public void setNO(String NO) { this.NO = NO;}
 
-    public String getISBN() {
-        return ISBN;
-    }
+    public String getISBN() { return ISBN; }
 
-    public void setISBN(String ISBN) {
-        this.ISBN = ISBN;
-    }
+    public void setISBN(String ISBN) { this.ISBN = ISBN; }
 
-    public String getTitulo() {
-        return titulo;
-    }
+    public String getTitulo() { return titulo; }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
 
-    public String getAutor() {
-        return autor;
-    }
+    public String getAutor() { return autor; }
 
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
+    public void setAutor(String autor) { this.autor = autor; }
 
-    public String getEditorial() {
-        return editorial;
-    }
+    public String getEditorial() { return editorial; }
 
-    public void setEditorial(String editorial) {
-        this.editorial = editorial;
-    }
+    public void setEditorial(String editorial) { this.editorial = editorial; }
 
-    public String getDescripcion() {
-        return descripcion;
-    }
+    public String getDescripcion() { return descripcion; }
 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
 
-    public String getCategoria() {
-        return categoria;
-    }
+    public String getCategoria() { return categoria; }
 
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
+    public void setCategoria(String categoria) { this.categoria = categoria; }
 }
