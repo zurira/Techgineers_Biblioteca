635971246cbe41724f40c5655f77cd7ddf155a75
lizzy <20243ds052@utez.edu.mx>
2025-07-25 22:01:00 -0600

    cambios en el archivo controller de editar usuario

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/EditarUsuarioController.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/EditarUsuarioController.java
@@ -1,5 +1,26 @@
 package mx.edu.utez.biblioteca.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 public class EditarUsuarioController {
+    @FXML private TextField txtNombre, txtEmail, txtTelefono;
+    @FXML private DatePicker dateNacimiento;
+    @FXML private TextArea txtDireccion;
 
+    @FXML
+    private void guardar() {
+        System.out.println("Guardado: " + txtNombre.getText());
+        cerrar();
+    }
+
+    @FXML
+    private void cancelar() {
+        cerrar();
+    }
+
+    private void cerrar() {
+        Stage stage = (Stage) txtNombre.getScene().getWindow();
+        stage.close();
+    }
 }
