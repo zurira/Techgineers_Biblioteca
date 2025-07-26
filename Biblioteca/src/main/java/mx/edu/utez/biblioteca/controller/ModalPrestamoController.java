d5ee9296f8d775e646f91bd963aef422c14ed5e9
zurira <20243ds036@utez.edu.mx>
2025-07-26 06:47:17 -0600

    Se instanciaron las clases: UsuarioDaoImpl, EjemplarDaoImpl, PrestamoDaoImpl y DetallePrestamoDaoImpl ya que serán de utilidad para realizar las acciones del controllador

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/ModalPrestamoController.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/ModalPrestamoController.java
@@ -4,6 +4,10 @@ package mx.edu.utez.biblioteca.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import mx.edu.utez.biblioteca.dao.impl.DetallePrestamoDaoImpl;
+import mx.edu.utez.biblioteca.dao.impl.EjemplarDaoImpl;
+import mx.edu.utez.biblioteca.dao.impl.PrestamoDaoImpl;
+import mx.edu.utez.biblioteca.dao.impl.UsuarioDaoImpl;
 import mx.edu.utez.biblioteca.model.Ejemplar;
 
 public class ModalPrestamoController {
@@ -16,6 +20,10 @@ public class ModalPrestamoController {
     @FXML private TableColumn<Ejemplar, String> colCodigo, colTitulo, colUbicacion;
     @FXML private TableColumn<Ejemplar, Boolean> colSeleccionar;
 
+    private final UsuarioDaoImpl usuarioDAO = new UsuarioDaoImpl();
+    private final EjemplarDaoImpl ejemplarDAO = new EjemplarDaoImpl();
+    private final PrestamoDaoImpl prestamoDAO = new PrestamoDaoImpl();
+    private final DetallePrestamoDaoImpl detalleDAO = new DetallePrestamoDaoImpl();
 
     // Aquí voy a agregar los  métodos para guardar o cancelar
 }
