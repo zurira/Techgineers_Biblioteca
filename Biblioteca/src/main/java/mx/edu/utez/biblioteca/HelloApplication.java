2116f078ade06e00b636abf40d38141f6b64c77f
Daniela <20243ds043@utez.edu.mx>
2025-07-27 17:33:44 -0600

    Se modifico la ruta para cargar la vista y se cambio el tamaño al correr la vista

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/HelloApplication.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/HelloApplication.java
@@ -2,6 +2,7 @@ package mx.edu.utez.biblioteca;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,10 +10,11 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {//se estructuraron las carpetas
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/mx/edu/utez/biblioteca/views/Usuarios.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("Prestamos de libros");
         stage.setScene(scene);
         stage.show();
     }
