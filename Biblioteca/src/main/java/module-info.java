c0e5f75245f4f73c099996152f08268659341094
Daniela <20243ds043@utez.edu.mx>
2025-07-27 17:42:36 -0600

    Implementación y configuración de la funcionalidad para la gestión de Usuarios a través de module-info.java

--- Biblioteca/src/main/java/module-info.java
+++ Biblioteca/src/main/java/module-info.java
@@ -5,7 +5,12 @@ module mx.edu.utez.biblioteca {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
+    requires org.kordamp.ikonli.javafx;
 
     opens mx.edu.utez.biblioteca to javafx.fxml;
+    opens mx.edu.utez.biblioteca.controller to javafx.fxml;
+
     exports mx.edu.utez.biblioteca;
+    exports mx.edu.utez.biblioteca.controller;
+    opens mx.edu.utez.biblioteca.model to javafx.base;
 }
