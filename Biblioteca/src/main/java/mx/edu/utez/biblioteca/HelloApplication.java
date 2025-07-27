f3b9f4c6c46ca530ec6f70cedc33a77f2621bab2
Daniela <20243ds043@utez.edu.mx>
2025-07-26 18:44:19 -0600

    Método principal para cargar la interfaz visual de la pantalla PRESTAMOS

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/HelloApplication.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/HelloApplication.java
@@ -4,15 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 
-import java.io.IOException;
+public class HelloApplication extends Application {
 
-public class HelloApplication extends Application {//se estructuraron las carpetas
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/mx/edu/utez/biblioteca/views/Prestamo.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("Prestamos de libros");
         stage.setScene(scene);
         stage.show();
     }
