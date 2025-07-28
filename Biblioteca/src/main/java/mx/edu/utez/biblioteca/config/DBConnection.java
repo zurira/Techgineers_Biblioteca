6d3f09ad6c9e197c887ee2c842d410c3b028aa5a
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-27 19:06:28 -0600

    Cambie, la ruta del wallet y algunas cosas mas

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/config/DBConnection.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/config/DBConnection.java
@@ -1,26 +1,31 @@
 package mx.edu.utez.biblioteca.config;
 
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    public static Connection getConnection() {
-        try {
-            String alias = "bdutez_high"; // Usa el alias definido en tu tnsnames.ora
-            String tnsAdminPath = "C:\\Users\\letic\\Downloads\\Wallet_L9I11J33YFKVI0T1"; // ruta donde se descomprime la wallet
 
-            String url = "jdbc:oracle:thin:@"+alias+"?TNS_ADMIN="+tnsAdminPath;
-            String user = "L9I11J33YFKVI0T1"; //  usuario
-            String pass = "Techgineers123"; //  contraseña
+    //falta que le cambie lo de la URL
+    private static final String URL      = "jdbc:oracle:thin:@l9i11j33yfkvi0t1_low";
+    private static final String USER     = "ADMIN";
+    private static final String PASSWORD = "Tech$Biblio2025";
 
-            return DriverManager.getConnection(url, user, pass);
+    // Obtiene una conexión nueva
+    public static Connection getConnection() throws SQLException {
+        // 1. Apunta al directorio donde descomprimiste el wallet
+        System.setProperty("oracle.net.tns_admin", "C:\\Users\\letic\\Downloads\\Wallet_VYG59TE11JX0QYGS");
+        // 2. (Opcional) fuerza la validación de nombre de servidor en el certificado
+        System.setProperty("oracle.net.ssl_server_dn_match", "true");
+        // 3. Obtiene la conexión usando alias, user y pass
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void main(String[] args) throws SQLException {
+        try (Connection conn = getConnection()) {
+            System.out.println("¡Conexión exitosa!");
         } catch (SQLException e) {
-            System.err.println(" Error al conectar a Oracle con Wallet:");
             e.printStackTrace();
-            return null;
         }
     }
 }
