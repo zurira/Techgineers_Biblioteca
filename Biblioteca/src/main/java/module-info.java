0b5bf8c71978e6f6973d1c61d9fcef7d9131d33f
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-27 19:28:05 -0600

    Agregue lineas para que compilara

--- Biblioteca/src/main/java/module-info.java
+++ Biblioteca/src/main/java/module-info.java
@@ -1,12 +1,15 @@
 module mx.edu.utez.biblioteca {
+
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
 
-    opens mx.edu.utez.biblioteca to javafx.fxml;
     exports mx.edu.utez.biblioteca;
+    exports mx.edu.utez.biblioteca.model; // 👈 necesario para evitar el error
+    opens mx.edu.utez.biblioteca to javafx.fxml;
     opens mx.edu.utez.biblioteca.controller to javafx.fxml;
+    opens mx.edu.utez.biblioteca.model to javafx.fxml; // 👈 si usas FXML en el modal
 }
+
