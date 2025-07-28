501577143c26e5192e2a948b0c90635b6b5e7172
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-27 19:28:53 -0600

    Modifique para que fuera funcional

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/ModalCerrarSesionController.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/ModalCerrarSesionController.java
@@ -1,7 +1,28 @@
 package mx.edu.utez.biblioteca.controller;
 
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class ModalCerrarSesionController {
-    //aqui voy a poner la logica que se va a seguir para el correcto funcionamiento de los botones
+    @FXML private Button aceptarButton;
+    @FXML private Button cancelarButton;
+
+    public static boolean cerrarSesionConfirmado = false;
+
+    @FXML
+    public void initialize() {
+        cancelarButton.setOnAction(event -> {
+            cerrarSesionConfirmado = false;
+            Stage stage = (Stage) cancelarButton.getScene().getWindow();
+            stage.close();
+        });
+
+        aceptarButton.setOnAction(event -> {
+            cerrarSesionConfirmado = true;
+            Stage stage = (Stage) aceptarButton.getScene().getWindow();
+            stage.close();
+        });
+    }
 }
