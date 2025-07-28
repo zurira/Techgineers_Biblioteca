7d155ef1eaa2123665e520294e12329d9cab1466
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-27 19:09:15 -0600

    Realize los gett

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Administrador.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Administrador.java
@@ -1,5 +1,5 @@
-package mx.edu.utez.biblioteca.model;
 
+package mx.edu.utez.biblioteca.model;
 
 public class Administrador {
     private int id;
@@ -8,16 +8,25 @@ public class Administrador {
     private String correo;
     private boolean estado;
 
-
-        public Administrador(int id, String nombreCompleto, String usuario, String correo, boolean estado) {
-            this.id = id;
-            this.nombreCompleto = nombreCompleto;
-            this.usuario = usuario;
-            this.correo = correo;
-            this.estado = estado;
-        }
+    public Administrador(int id, String nombreCompleto, String usuario, String correo, boolean estado) {
+        this.id = id;
+        this.nombreCompleto = nombreCompleto;
+        this.usuario = usuario;
+        this.correo = correo;
+        this.estado = estado;
     }
 
+    public int getId() { return id; }
+    public String getNombreCompleto() { return nombreCompleto; }
+    public String getUsuario() { return usuario; }
+    public String getCorreo() { return correo; }
+    public boolean getEstado() { return estado; }
 
+    public void setId(int id) { this.id = id; }
+    public void setNombreCompleto(String nombreCompleto) { this.nombreCompleto = nombreCompleto; }
+    public void setUsuario(String usuario) { this.usuario = usuario; }
+    public void setCorreo(String correo) { this.correo = correo; }
+    public void setEstado(boolean estado) { this.estado = estado; }
+}
 
 
