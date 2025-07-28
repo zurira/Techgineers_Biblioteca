e2f99ee17d5278026fe6e30e9d481d4bb5fd058c
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-28 13:17:50 -0600

    Coloque los atributos de la clase administrador

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Administrador.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Administrador.java
@@ -1,32 +1,15 @@
 
 package mx.edu.utez.biblioteca.model;
 
+
 public class Administrador {
     private int id;
     private String nombreCompleto;
     private String usuario;
     private String correo;
+    private String telefono;
+    private String contrasena;
+    private String rol;
+    private String direccion;
+    private String imagen; // puedes usar String para la ruta o cambiarlo por byte[] si usas BLOB
     private boolean estado;
-
-    public Administrador(int id, String nombreCompleto, String usuario, String correo, boolean estado) {
-        this.id = id;
-        this.nombreCompleto = nombreCompleto;
-        this.usuario = usuario;
-        this.correo = correo;
-        this.estado = estado;
-    }
-
-    public int getId() { return id; }
-    public String getNombreCompleto() { return nombreCompleto; }
-    public String getUsuario() { return usuario; }
-    public String getCorreo() { return correo; }
-    public boolean getEstado() { return estado; }
-
-    public void setId(int id) { this.id = id; }
-    public void setNombreCompleto(String nombreCompleto) { this.nombreCompleto = nombreCompleto; }
-    public void setUsuario(String usuario) { this.usuario = usuario; }
-    public void setCorreo(String correo) { this.correo = correo; }
-    public void setEstado(boolean estado) { this.estado = estado; }
-}
-
-
