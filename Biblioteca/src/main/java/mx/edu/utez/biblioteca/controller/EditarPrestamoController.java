e437560e1cd943080812dcca105fc21df64ca842
zurira <20243ds036@utez.edu.mx>
2025-07-28 07:14:31 -0600

    Se enlazaron los elemntos a ocupar del fxml, se crearon instancias de las clases a utilizar

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/EditarPrestamoController.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/EditarPrestamoController.java
@@ -1,4 +1,36 @@
 package mx.edu.utez.biblioteca.controller;
 
-public class EditarPrestamoController {
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import mx.edu.utez.biblioteca.dao.impl.DetallePrestamoDaoImpl;
+import mx.edu.utez.biblioteca.dao.impl.EjemplarDaoImpl;
+import mx.edu.utez.biblioteca.dao.impl.PrestamoDaoImpl;
+import mx.edu.utez.biblioteca.model.Prestamo;
+import mx.edu.utez.biblioteca.model.UsuarioBiblioteca;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class EditarPrestamoController implements Initializable {
+
+   @FXML private DatePicker dpFechaPrestamo;
+   @FXML private DatePicker dpFechaLimite;
+   @FXML private DatePicker dpFechaDevolucion;
+   @FXML private ComboBox<String> cbEstado;
+   @FXML private ComboBox<UsuarioBiblioteca> comboBoxUsuarios;
+
+   private Prestamo prestamoActual;
+   private final PrestamoDaoImpl prestamoDao = new PrestamoDaoImpl();
+   private final EjemplarDaoImpl ejemplarDao = new EjemplarDaoImpl();
+   private final DetallePrestamoDaoImpl detalleDao = new DetallePrestamoDaoImpl();
+
+   @Override
+   public void initialize(URL url, ResourceBundle resourceBundle) {
+       // Estados
+       cbEstado.setItems(FXCollections.observableArrayList("Activo", "Finalizado", "Retrasado"));
+   }
+
 }
