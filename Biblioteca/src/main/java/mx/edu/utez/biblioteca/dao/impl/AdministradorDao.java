ccbf0a02e6ecb12871805add149a778db98bc5cb
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-27 19:08:21 -0600

    Realize cambios para ya poder hacerla funcional

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/impl/AdministradorDao.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/impl/AdministradorDao.java
@@ -1,5 +1,7 @@
 package mx.edu.utez.biblioteca.dao.impl;
 
+
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableView;
@@ -9,7 +11,6 @@ import mx.edu.utez.biblioteca.model.Administrador;
 import java.sql.*;
 
 public class AdministradorDao {
-
     public static void cargarAdministradores(TableView<Administrador> tabla) {
         tabla.setItems(getListaAdmins(""));
     }
@@ -21,7 +22,12 @@ public class AdministradorDao {
     private static ObservableList<Administrador> getListaAdmins(String filtro) {
         ObservableList<Administrador> lista = FXCollections.observableArrayList();
         try (Connection conn = DBConnection.getConnection()) {
-            String sql = "SELECT ID, NOMBRE_COMPLETO, USUARIO, CORREO, ESTADO FROM ADMINISTRADORES WHERE LOWER(NOMBRE_COMPLETO) LIKE ?";
+            String sql = """
+                SELECT ID, NOMBRE, USERNAME, CORREO, ESTADO
+                FROM USUARIO_SISTEMA
+                WHERE LOWER(NOMBRE) LIKE ?
+                AND ID_ROL = (SELECT ID FROM ROL WHERE NOMBRE = 'Administrador')
+            """;
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setString(1, "%" + filtro.toLowerCase() + "%");
             ResultSet rs = stmt.executeQuery();
@@ -29,10 +35,10 @@ public class AdministradorDao {
             while (rs.next()) {
                 lista.add(new Administrador(
                         rs.getInt("ID"),
-                        rs.getString("NOMBRE_COMPLETO"),
-                        rs.getString("USUARIO"),
+                        rs.getString("NOMBRE"),
+                        rs.getString("USERNAME"),
                         rs.getString("CORREO"),
-                        rs.getBoolean("ESTADO")
+                        rs.getString("ESTADO").equals("S")
                 ));
             }
         } catch (Exception e) {
