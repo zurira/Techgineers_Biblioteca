2dce765101adff131d9d33ab8e678dbe7c134749
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-28 13:11:38 -0600

    Modifique el moduleinfo o

--- Biblioteca/src/main/java/module-info.java
+++ Biblioteca/src/main/java/module-info.java
@@ -4,10 +4,10 @@ module mx.edu.utez.biblioteca {
     requires javafx.fxml;
     requires java.sql;
     requires javafx.graphics;
-
     opens mx.edu.utez.biblioteca.controller to javafx.fxml;
     opens mx.edu.utez.biblioteca to javafx.graphics;
 
     exports mx.edu.utez.biblioteca;
     exports mx.edu.utez.biblioteca.controller;
+
 }
