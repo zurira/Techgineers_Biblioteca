551839cfac556c4d820385f0e10708a9584cbeee
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-28 13:19:04 -0600

    Coloque el constructor de de la clase Administrador

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Administrador.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Administrador.java
@@ -13,3 +13,19 @@ public class Administrador {
     private String direccion;
     private String imagen; // puedes usar String para la ruta o cambiarlo por byte[] si usas BLOB
     private boolean estado;
+
+    public Administrador(int id, String nombreCompleto, String usuario, String correo, String telefono,
+                         String contrasena, String rol, String direccion, String imagen, boolean estado) {
+        this.id = id;
+        this.nombreCompleto = nombreCompleto;
+        this.usuario = usuario;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.contrasena = contrasena;
+        this.rol = rol;
+        this.direccion = direccion;
+        this.imagen = imagen;
+        this.estado = estado;
+    }
+
+
