b3be0fca3106ee94e7ec78275975f554e9ef7b82
lizzy <20243ds052@utez.edu.mx>
2025-07-29 08:04:39 -0600

    se hicieron modificaciones en module-info.java

--- Biblioteca/src/main/java/module-info.java
+++ Biblioteca/src/main/java/module-info.java
@@ -1,20 +1,20 @@
 module mx.edu.utez.biblioteca {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
-    requires java.sql;
     requires org.kordamp.ikonli.javafx;
+    requires java.sql;
     requires java.desktop;
 
-
+    // Indica qué paquetes se abren para reflexión a javafx.fxml
     opens mx.edu.utez.biblioteca to javafx.fxml;
     opens mx.edu.utez.biblioteca.controller to javafx.fxml;
 
+    // Exporta paquetes públicos si deseas usarlos desde otros módulos
     exports mx.edu.utez.biblioteca;
-
     exports mx.edu.utez.biblioteca.controller;
-    opens mx.edu.utez.biblioteca.model to javafx.base;
 
+    // Abre el paquete model si haces bindings o reflexión desde javafx.base
+    opens mx.edu.utez.biblioteca.model to javafx.base;
 }
