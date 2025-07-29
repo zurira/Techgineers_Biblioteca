d1509fe255fe3c3c3511ef840c07ccacada86c0e
lizzy <20243ds052@utez.edu.mx>
2025-07-29 12:02:41 -0600

    modal funcional, botón de guardar ya incluido y funcionando correctamente

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/HelloApplication.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/HelloApplication.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.net.URL;
 
-
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
