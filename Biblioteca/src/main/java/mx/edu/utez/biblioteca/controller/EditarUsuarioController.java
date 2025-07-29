a3084620cf4a977c2e06b882bf085b25960643e2
lizzy <20243ds052@utez.edu.mx>
2025-07-28 20:06:10 -0600

    se importaron clases fxml

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/EditarUsuarioController.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/EditarUsuarioController.java
@@ -4,9 +4,12 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 public class EditarUsuarioController {
-    @FXML private TextField txtNombre, txtEmail, txtTelefono;
-    @FXML private DatePicker dateNacimiento;
-    @FXML private TextArea txtDireccion;
+    @FXML
+    private TextField txtNombre, txtEmail, txtTelefono;
+    @FXML
+    private DatePicker dateNacimiento;
+    @FXML
+    private TextArea txtDireccion;
 
     @FXML
     private void guardar() {
@@ -23,3 +26,4 @@ public class EditarUsuarioController {
         Stage stage = (Stage) txtNombre.getScene().getWindow();
         stage.close();
     }
+}
