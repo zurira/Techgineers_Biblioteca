977250f6300622d21d0c80c436a57442d3bccdc3
Daniela <20243ds043@utez.edu.mx>
2025-07-29 22:26:49 -0600

    Se modifico la ruta para la visualización del fxml

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/HelloApplication.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/HelloApplication.java
@@ -6,14 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/mx/edu/utez/biblioteca/views/estadisticas.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/mx/edu/utez/biblioteca/views/Estadisticas.fxml"));
         Scene scene = new Scene(root, 1024, 640);
-        stage.setTitle("Biblioteca");
+        stage.setTitle("Estadisticas");
         stage.setScene(scene);
         stage.show();
     }
