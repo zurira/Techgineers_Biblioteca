d1a21ade125a9c8afd0991701152723cac97fec7
TaniaSR8 <20243ds011@utez.edu.mx>
2025-07-30 10:57:50 -0600

    Agregue insertarAdministrador, para que se visualice los registros de los usuarios

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/impl/AdministradorDao.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/impl/AdministradorDao.java
@@ -8,6 +8,7 @@ import javafx.scene.control.TableView;
 import mx.edu.utez.biblioteca.config.DBConnection;
 import mx.edu.utez.biblioteca.model.Administrador;
 
+import java.io.InputStream;
 import java.sql.*;
 
 public class AdministradorDao {
@@ -46,4 +47,33 @@ public class AdministradorDao {
         }
         return lista;
     }
+
+    //Agrego esto nuevo
+
+    public static boolean insertarAdministrador(
+            String nombre, String usuario, String correo, String telefono,
+            String contrasena, InputStream foto, int idRol, String estado, String direccion
+    ) {
+        String sql = "INSERT INTO USUARIO_SISTEMA (NOMBRE, USERNAME, CORREO, TELEFONO, PASSWORD, FOTO, ID_ROL, ESTADO, DIRECCION) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, nombre);
+            stmt.setString(2, usuario);
+            stmt.setString(3, correo);
+            stmt.setString(4, telefono);
+            stmt.setString(5, contrasena);
+            stmt.setBlob(6, foto);
+            stmt.setInt(7, idRol);
+            stmt.setString(8, estado);
+            stmt.setString(9, direccion);
+
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
