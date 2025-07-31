e8a71b2eb1c72fcd2e631bb74b024bec21dce8cb
vivisahe <20243ds058@utez.edu.mx>
2025-07-31 17:34:46 -0600

    IBiblioteca Actualizado

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/IBibliotecario.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/IBibliotecario.java
@@ -1,11 +1,17 @@
 package mx.edu.utez.biblioteca.dao;
 
+import mx.edu.utez.biblioteca.model.Bibliotecario;
+import java.util.List;
+
 public interface IBibliotecario {
-    List<Bibliotecario> findAll()throws Exception;
-    Bibliotecario findBy(int id)throws Exception;
-    void create(Bibliotecario bibliotecario) throws Exception;
-    void update(Bibliotecario bibliotecario) throws Exception;
-    void delete(int id) throws Exception;
+    List<Bibliotecario> findAll() throws Exception;
+    Bibliotecario findById(int id) throws Exception;
+    boolean create(Bibliotecario bibliotecario) throws Exception;
+    boolean update(Bibliotecario bibliotecario) throws Exception;
+    boolean delete(int id) throws Exception;
     List<Bibliotecario> search(String searchTerm) throws Exception;
     boolean updateStatus(int idBibliotecario, String estado) throws Exception;
+
+    // Método para buscar un bibliotecario por su username para el login
+    Bibliotecario findByUsername(String username) throws Exception;
 }
