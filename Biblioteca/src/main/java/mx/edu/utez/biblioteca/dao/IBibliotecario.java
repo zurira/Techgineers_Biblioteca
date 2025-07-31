4ef48e4723b915dd8a37f136d9a28a263122112b
vivisahe <20243ds058@utez.edu.mx>
2025-07-31 10:31:33 -0600

    Se empezo agregar codigo al IBibliotecario(findAll,finfBy,create,update,delete,search y updatestatus)

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/IBibliotecario.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/dao/IBibliotecario.java
@@ -1,4 +1,11 @@
 package mx.edu.utez.biblioteca.dao;
 
 public interface IBibliotecario {
+    List<Bibliotecario> findAll()throws Exception;
+    Bibliotecario findBy(int id)throws Exception;
+    void create(Bibliotecario bibliotecario) throws Exception;
+    void update(Bibliotecario bibliotecario) throws Exception;
+    void delete(int id) throws Exception;
+    List<Bibliotecario> search(String searchTerm) throws Exception;
+    boolean updateStatus(int idBibliotecario, String estado) throws Exception;
 }
