067bfd1604abc2ea40e5044faa069723f900001d
zurira <20243ds036@utez.edu.mx>
2025-08-01 11:32:32 -0600

    Se modifico la ruta del wallet

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/config/DBConnection.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/config/DBConnection.java
@@ -7,14 +7,14 @@ import java.sql.SQLException;
 public class DBConnection {
 
 
-    private static final String URL      = "jdbc:oracle:thin:@l9i11j33yfkvi0t1_low";
+    private static final String URL      = "jdbc:oracle:thin:@vyg59te11jx0qygs_low";
     private static final String USER     = "ADMIN";
-    private static final String PASSWORD = "Techgineers123";
+    private static final String PASSWORD = "Tech$Biblio2025";
 
     // Obtiene una conexión nueva
     public static Connection getConnection() throws SQLException {
         // 1. Apunta al directorio donde descomprimiste el wallet
-        System.setProperty("oracle.net.tns_admin", "F:\\Downloads\\Wallet_L9I11J33YFKVI0T1");
+        System.setProperty("oracle.net.tns_admin", "F:\\Wallet_VYG59TE11JX0QYGS");
         // 2. (Opcional) fuerza la validación de nombre de servidor en el certificado
         System.setProperty("oracle.net.ssl_server_dn_match", "true");
         // 3. Obtiene la conexión usando alias, user y pass
