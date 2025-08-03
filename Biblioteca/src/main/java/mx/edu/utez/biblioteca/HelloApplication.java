ef9f4c40120f32c130e23e48c593f15c0b19e6fd
Daniela <20243ds043@utez.edu.mx>
2025-08-02 20:23:09 -0600

    Cambio para que al abrir maximice toda la ventana

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/HelloApplication.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/HelloApplication.java
@@ -4,25 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
-
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/mx/edu/utez/biblioteca/views/Usuarios.fxml"));
-        Scene scene = new Scene(root, 1280, 720);
-        stage.setTitle("Biblioteca");
+        Scene scene = new Scene(root);
+        stage.setTitle("Prestamos");
         stage.setScene(scene);
+        // Esta línea maximiza la ventana.
+        stage.setMaximized(true);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
     }
-
 }
