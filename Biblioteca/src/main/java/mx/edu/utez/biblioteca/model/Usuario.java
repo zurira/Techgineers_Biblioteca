eb0a7023b5add0ba56de3c672bb4a6def8ee71a6
vivisahe <20243ds058@utez.edu.mx>
2025-08-03 01:28:09 -0600

    se creo la clase de Usuario.java para contener la informacion de los usuarios y poder usarla en las diferentes capas de la app

---
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/model/Usuario.java
@@ -0,0 +1,111 @@
+package mx.edu.utez.biblioteca.model;
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String correo;
+    private String telefono;
+    private String username;
+    private String password;
+    private Rol rol;
+    private String direccion;
+    private String estado;
+    private byte[] foto;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String nombre, String telefono, String correo, String username, String password, Rol rol, String direccion, String estado, byte[] foto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.username = username;
+        this.password = password;
+        this.rol = rol;
+        this.direccion = direccion;
+        this.estado = estado;
+        this.foto = foto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public byte[] getFoto() {
+        return foto;
+    }
+
+    public void setFoto(byte[] foto) {
+        this.foto=foto;
+    }
+}
+
