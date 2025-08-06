eb041ff4ea21b230a7c72d7cb7706258a3222be8
zurira <20243ds036@utez.edu.mx>
2025-08-06 04:17:20 -0600

    Se inicio con la implementación del controller

---
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/controller/EditarBibliotecarioController.java
@@ -0,0 +1,24 @@
+package mx.edu.utez.biblioteca.controller;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditarBibliotecarioController implements Initializable {
+
+    @FXML private TextField txtNombre, txtCorreo, txtTelefono, txtUsuario;
+    @FXML private PasswordField txtPassword;
+    @FXML private TextField txtPasswordVisible;
+    @FXML private Button togglePasswordBtn;
+    @FXML private ComboBox<String> comboEstado;
+    @FXML private TextArea txtDireccion;
+    @FXML private ImageView imageView;
+    @FXML private Button btnSeleccionarImagen;
+    @FXML private Button btnGuardar, btnCancelar;
+
+
+}
