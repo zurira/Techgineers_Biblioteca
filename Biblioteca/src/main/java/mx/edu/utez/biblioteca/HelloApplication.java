2f757755c170814a3b83acad20730e8d2e975e71
vivisahe <20243ds@utez.edu.mx>
2025-08-08 01:13:09 -0600

    Se termino la vista de libros del usuario Administradores

--- Biblioteca/src/main/java/mx/edu/utez/biblioteca/HelloApplication.java
+++ Biblioteca/src/main/java/mx/edu/utez/biblioteca/HelloApplication.java
@@ -7,7 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
+//Vista terminada
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
